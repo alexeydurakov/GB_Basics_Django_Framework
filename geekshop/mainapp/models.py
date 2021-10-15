40b7f2a8b1b09cf9c48337dbbbb4d2952290d0cb
alexeydurakov <alexey.durakov@yandex.ru>
2021-10-15 18:01:43 +0300

    Lesson_3

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(
+        max_length=64,
+        verbose_name='имя',
+        unique=True,
+    )
+
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
