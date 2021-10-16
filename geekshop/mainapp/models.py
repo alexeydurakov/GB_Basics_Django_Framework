00270c1c534c6320a31bd5bd2b21f0de1ad9db6d
alexeydurakov <alexey.durakov@yandex.ru>
2021-10-16 16:07:17 +0300

    Lesson_3_add

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -22,3 +22,52 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(
+        verbose_name='имя продукта',
+        max_length=128,
+    )
+
+    image = models.ImageField(
+        upload_to='products_images',
+        blank=True,
+    )
+
+    short_desc = models.CharField(
+        verbose_name='краткое описание',
+        max_length=60,
+        blank=True,
+    )
+
+    description = models.TextField(
+        verbose_name='описание продукта',
+        blank=True,
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        verbose_name='цена',
+        decimal_places=2,
+    )
+
+    quantity = models.PositiveIntegerField(
+        verbose_name='количество на складе',
+        default=0,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
