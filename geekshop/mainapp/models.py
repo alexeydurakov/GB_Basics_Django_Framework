3eaa53088303babc085be4162dc47ba6a4d09533
alexeydurakov <alexey.durakov@yandex.ru>
2021-10-16 16:17:57 +0300

    Lesson_3_add2

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -71,3 +71,9 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
+
+
+class Meta:
+    ordering = ['-updated']
+    verbose_name = 'товар'
+    verbose_name_plural = 'товары'
